cbdae3903dab6501a46eae89fdc7ff4bf2f9c9c4
Jason Crockett <JesusisLord33333@outlook.com>
2021-09-29 15:45:52 +0100

    first try installing the mycroft skill

--- __init__.py
+++ __init__.py
@@ -6,21 +6,35 @@
 # libraries.  If you use an external library, be sure to include it
 # in the requirements.txt file so the library is installed properly
 # when the skill gets installed later by a user.
+import os
+import random
+import json
 
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler, intent_file_handler
 from mycroft.util.log import LOG
+from mycroft.util import play_mp3
+from mycroft.audio import is_speaking, wait_while_speaking
 
 # Each skill is contained within its own class, which inherits base methods
 # from the MycroftSkill class.  You extend this class as shown below.
 
+# a big thanks to the jarbisAI person for the study material (the laugh skill)
 
 class NIVReaderSkill(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
         super(NIVReaderSkill, self).__init__(name="NIVReaderSkill")
-        
+        # get playlist
+        self.audio_bible_folder = '~/Music/NIV/'
+        self.player = False
+        self.playlist = open('playlist.list', 'r').readlines()
+        for index, line in self.playlist:
+            self.playlist[index] = line.replace(' ','\ ')
+            self.playlist[index] = line.rstrip()
+
+
         # Initialize working variables used within the skill.
         # self.count = 0
 
@@ -59,16 +73,23 @@ class NIVReaderSkill(MycroftSkill):
     # def stop(self):
     #    return False
 
-    @intent_file_handler('play.me.a.book.of.the.bible.intent')
+
+
+    @intent_file_handler('play.me.a.chapter.of.the.bible.intent')
     def handle_random_bible_book_intent(self):
-        pass
+        filename = self.audio_bible_folder + str(random.choice(self.playlist))
+        if is_speaking():
+            wait_while_speaking()
+        self.player = play_mp3(filename)
+
 
     def stop(self):
         try:
-            return true
+            if self.player.is_playing():
+                self.player.terminate()
+            return True
         except(Exception):
-            return false #pass the crackpipe lol - of coarse its gonna return true.
-
+            return False #pass the crackpipe lol - of coarse its gonna return true.
 
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
