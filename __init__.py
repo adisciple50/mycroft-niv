7e473cea1f35097f895e1e7826a8b86829b6f7c1
Jason Crockett <JesusisLord33333@outlook.com>
2021-10-01 07:08:05 +0100

    desperately trying to get mycroft to read the mp3 manifest

--- __init__.py
+++ __init__.py
@@ -31,13 +31,13 @@ class NIVReaderSkill(MycroftSkill):
         self.audio_bible_folder = '~/Music/NIV/'
         self.playlist = os.listdir('~/Music/NIV/')
         self.player = False
-        self.playlist_filename = './playlist.list'
+        self.playlist_filename = self.audio_bible_folder + 'playlist.list'
         with open(self.playlist_filename, 'w') as playlist_file:
             for index, line in self.playlist:
                 self.playlist[index] = line.replace(' ','\ ')
                 self.playlist[index] = line.rstrip()
             playlist_file.writelines()
-        self.playlist = open('./playlist.list', 'r').readlines()
+        self.playlist = open(self.playlist_filename, 'r').readlines()
 
         # Initialize working variables used within the skill.
         # self.count = 0
